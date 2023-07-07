231fe9686199d25d4cac652405ee9e7e485129ce
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-07 16:20:21 +0000

    Fix increment bug in code

--- my-app/src/components/StatefulGreetingWithPrevState.js
+++ my-app/src/components/StatefulGreetingWithPrevState.js
@@ -1,43 +1,59 @@
 import React from "react";
 
 class StatefulGreetingWithPrevState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      introduction: "Hello!",
+      buttonText: "Exit",
+      count: 0,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            introduction: "Hello!",
-            buttonText: "Exit",
-            count: 0,
-        };
-    }
+  handleClick() {
+    this.setState((prevState, prevProps) => {
+      console.log("Previous State:", prevState);
+      console.log("Previous Props:", prevProps);
+      return {
+        introduction:
+          prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+        buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+      };
+    });
+  }
 
-    handleClick() {
-        this.setState({
-            introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
-            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
-            count: this.state.count + 1,
-        }, () => {
-            // Callback function:
+  increment() {
+    this.setState((prevState, prevProps) => {
+      console.log("Previous State:", prevState);
+      console.log("Previous Props:", prevProps);
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
 
-        console.log("new state", this.state.introduction);
-        console.log("new state", this.state.buttonText);        
-        });
-        console.log(this.state.count);
-    }
+//   incrementFive() {
+//     this.increment();
+//     this.increment();
+//     this.increment();
+//     this.increment();
+//     this.increment();
+//   }
 
-
-    render() {
-        return (
-        <div>        
-            <h1>{this.state.introduction}</h1>
-            <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
-            <button onClick={() => this.handleClick()}>{this.state.count}</button>
-
-            <h2>You've clicked {this.state.count} times. </h2>
-        </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.introduction} {this.props.greeting}
+        </h1>
+        <button onClick={() => this.handleClick()}>
+          {this.state.buttonText}
+        </button>
+        <button onClick={() => this.increment()}>Increment</button>
+        <p>You've clicked {this.state.count} times</p>
+      </div>
+    );
+  }
 }
 
-export default StatefulGreetingWithPrevState
+export default StatefulGreetingWithPrevState;
