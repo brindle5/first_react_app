423705528a86303b3e69d9a0f7507247585ba51a
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-17 15:19:05 +0000

    Break down components into smaller components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,13 +8,13 @@ import EventsFunctional from './components/EventsFunctional';
 import EventBinding from './components/EventBinding';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import NestingComponents from './components/NestingComponents';
 
 
 
 function App() {
   return (<div className="App">
-      <ConditionalRenderingClass />
-      <ConditionalRenderingFunctional connected={true} />
+      <NestingComponents />
     </div>
   );
 }
