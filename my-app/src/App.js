4f35c256163527161f73cbc089b270256ff21322
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-17 15:37:27 +0000

    Add sign out buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,12 +9,14 @@ import EventBinding from './components/EventBinding';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsChild from './components/MethodsAsPropsChild';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 
 
 function App() {
   return (<div className="App">
-      <NestingComponents />
+       <MethodsAsPropsParent />
     </div>
   );
 }
