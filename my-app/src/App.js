49b3550cef29350c0668c2d7fb2cf5dadb60e4de
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-26 10:51:21 +0000

    Add search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,12 +17,13 @@ import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCycleCWU';
 import ControlledForm from './components/ControlledForm';
 import UncontrolledForm from './components/UncontrolledForm';
+import SearchBar from './components/SearchBar';
 
 
 
 function App() {
   return (<div className="App">
-      <UncontrolledForm />
+      <SearchBar />
     </div>
   );
 }
